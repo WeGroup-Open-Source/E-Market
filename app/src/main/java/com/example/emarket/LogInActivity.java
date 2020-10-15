99ff424cf0b2c38b21fa8db91f73fbd3b764bbeb
Alexis <alexismajyambere2@gmail.com>
2020-10-15 20:07:21 +0200

    [tast-#175281948] login and signup design

--- app/src/main/java/com/example/emarket/LogInActivity.java
+++ app/src/main/java/com/example/emarket/LogInActivity.java
@@ -2,6 +2,7 @@ package com.example.emarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LogInActivity extends AppCompatActivity {
@@ -10,5 +11,10 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+    }
+
+    public void openSignupPage(android.view.View view){
+        Intent signup=new Intent(this, SignUpActivity.class);
+        startActivity(signup);
     }
 }
